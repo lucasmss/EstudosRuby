89b0da5746811c9e6d6aecd00cafe979ac38346d
lucasmss16 <lucasmss16@gmail.com>
2020-07-10 16:47:57 -0300

    Ajustes do jogo, comentarios, ajuste de condiçoes melhora do jogo

--- Adivinha.rb
+++ Adivinha.rb
@@ -1,16 +1,37 @@
-puts 'Jogo de adivinhar!'
-puts 'Qual seu nome'
+#puts é a String do java, a forma de mostart a mensaagem na tela
+puts "Jogo de adivinhar!"
+puts "Qual seu nome"
+#gets é a forma de entrada, como voce vai interagir com o codigo
 nome = gets
-puts
-puts
-puts 'Vamos Jogar! ' + nome
-puts
-puts
-puts 'Escolhendo um número entre 0 e 200...'
-puts 'Escolhido! agora é sua vez'
+puts "\n\n" 
+#+ esta concatenando 
+puts "Vamos Jogar! " + nome
+puts "Escolhendo um número entre 0 e 200..."
+puts "Escolhido! agora é sua vez"
+#forma de atribuir valor ao objeto
 numeroEscolhido = 180
-puts 'Primeira tentativa'
-puts 'digite o seu chute ?'
-chute = gets
-puts 'Sera que voce acertou ??'
-puts chute.to_i == numeroEscolhido
+
+limiteDeTentativa = 5
+
+for tentativas in 1..limiteDeTentativa
+    puts "\n\n" 
+    puts "tentativa " + tentativas.to_s + " de " + limiteDeTentativa.to_s 
+    puts "digite o seu chute ?"
+    #forma de pular linha sem adicionar diversos puts
+    chute = gets
+    puts "Sera que voce acertou ??"
+    #forma de substituir o valor de texto string para int
+    #forma de condicionar a mensagem!
+    acertou = chute.to_i == numeroEscolhido 
+    if acertou
+        puts "Acertou !"
+        break
+    else
+        maior = numeroEscolhido > chute.to_i
+        if maior
+            puts "O numero é maior do que o chute"
+        else
+            puts "O numero é menor do que o chute"
+        end    
+    end
+end
